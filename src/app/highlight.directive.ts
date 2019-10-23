8ecf83b658a3e5183ae53d12b5b3279341525233
Adam Rosenberg <arosenberg@mastercontrol.com>
2019-10-23 14:25:32 -0600

    yay

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,16 +1,17 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class AppHighlightDirective {
+  @Input('appHighlightColor') color:string;
 
   @HostBinding('style.backgroundColor')
   backgroundColor;
 
   @HostListener('mouseover')
   onMouseOver() {
-    this.backgroundColor = '#F5F5F5'
+    this.backgroundColor = this.color;
   }
 
   @HostListener('mouseleave')
