0ec2cfbb9e9161ac764e9cd34b5d8b0ef8843ee6
Adam Rosenberg <arosenberg@mastercontrol.com>
2019-10-23 15:04:14 -0600

    refactored currency pipe to use an angular default pipe to help

--- src/app/currency-renderer.pipe.ts
+++ src/app/currency-renderer.pipe.ts
@@ -1,22 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
 
 @Pipe({
   name: 'currencyRenderer'
 })
 export class CurrencyRendererPipe implements PipeTransform {
 
-  transform(amount: number, currency:string='USD', exchangeRate: number = 1): any {
-    let currencySign = '$'
-    switch(currency) {
-      case 'EUR':
-        currencySign = '€';
-        break;
-      case 'GBP':
-        currencySign = '£';
-        break;
-    }
-
-    return currencySign + (amount * exchangeRate);
+  transform(value: number, currency:string='USD', exchangeRate: number = 1): any {
+    return new CurrencyPipe('en-us').transform(value * exchangeRate, currency,'symbol', '1.0-2')
   }
+
+
 
 }
