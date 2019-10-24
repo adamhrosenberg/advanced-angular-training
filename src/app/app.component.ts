80c8078c924b85ca30defa1ba9af31c29cad4031
Adam Rosenberg <arosenberg@mastercontrol.com>
2019-10-24 10:35:55 -0600

    added plate count fun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { map, tap } from 'rxjs/operators';
 import { Observable, Subscription } from 'rxjs/index';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import {LicensePlate} from './license-plate';
@@ -13,9 +14,14 @@ export class AppComponent {
   title: string = "title yo";
   description: string = "desc yo";
   licensePlates$: Observable<LicensePlate[]>;
+  plateCount: number = 0;
+
 
   constructor(private licensePlateService: LicensePlateService){
-    this.licensePlates$ = this.licensePlateService.getList();
+    this.licensePlates$ = this.licensePlateService.getList().pipe(
+      tap(plates => this.plateCount = plates.length)
+    )
+    // ^^ can also do this above tapping in the LP service to not couple the count with this component
   }
 
   isEven(index:number) {
