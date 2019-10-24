8baf23a048f40df14e652395bf6e2cab65af5a20
Adam Rosenberg <arosenberg@mastercontrol.com>
2019-10-24 10:23:27 -0600

    cleaned up app.components.ts for unused subs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,15 @@ import { LicensePlateService } from './license-plate.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnDestroy{
+export class AppComponent {
 
   title: string = "title yo";
   description: string = "desc yo";
   plates: LicensePlate[];
-  private subscription: Subscription;
   licensePlates$: Observable<LicensePlate[]>;
 
   constructor(private licensePlateService: LicensePlateService){
     this.licensePlates$ = this.licensePlateService.getList();
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
   }
 
   isEven(index:number) {
